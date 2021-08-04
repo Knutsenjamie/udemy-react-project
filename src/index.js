9debf19d1ca8eb40dd94c93513b2ff69f5df7525
“Jamie <knutsenjamie@yahoo.com>
2021-08-04 09:38:00 -0700

    add javascript variable for button instead as example

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+    const buttonText = 'Click Me!';
+    
     return (
         <div>
             <label className="label" for="name">Enter Stuff Here</label>
             <input id="name" type="text"/>
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>Send</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {buttonText}
+            </button>
         </div> 
     );
 };
