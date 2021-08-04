ead87a6917741cdef94ef31a6474037bd171762f
“Jamie <knutsenjamie@yahoo.com>
2021-08-04 09:51:00 -0700

    show correct syntax to use objects

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const App = () => {
             <label className="label" for="name">Enter Stuff Here</label>
             <input id="name" type="text"/>
             <button style={{ backgroundColor: 'blue', color: 'white' }}>
-                {buttonText}
-                {/* if you try to use an empty javascript object, you'll get the following error: "Objects are not valid as a React Child" */}
+                {buttonText.text}
+                {/* if you try to use an empty javascript object, you'll get the following error: "Objects are not valid as a React Child" UNLESS you add .text to the end of referenced object (see above ^)*/}
             </button>
         </div> 
     );
